811d3bffc4881cc5adecb0906b914b8c0a853bc2
CSIWill <91165874+CSIWill@users.noreply.github.com>
2021-10-26 20:01:04 -0400

    Trying SQLcipher

--- app/src/main/java/reflect_database/reflect_database.kt
+++ app/src/main/java/reflect_database/reflect_database.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import net.sqlcipher.database.SQLiteDatabase
 
 //exportSchema set to false as we only want 1 version of it for in-memory only case
 @Database(entities = [my_entry::class], version = 1, exportSchema = false)
