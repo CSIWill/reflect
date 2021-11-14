a673ea189a4042c8bff5ba18ea45b2964130e4e9
mbradley38 <91166207+mbradley38@users.noreply.github.com>
2021-11-14 13:40:03 -0500

    Add files via upload

--- app/src/main/java/com/example/reflect/EntryViewModel.kt
+++ app/src/main/java/com/example/reflect/EntryViewModel.kt
@@ -1,29 +1,15 @@
 package com.example.reflect
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import kotlinx.coroutines.flow.Flow
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
 import reflect_database.my_entry
+import reflect_database.reflect_repository
 
+class EntryViewModel(private val repository: reflect_repository) : ViewModel() {
 
-class EntryViewModel(private val repository: EntryRepository) : ViewModel() {
+    val allEntries: LiveData<List<my_entry>> = repository.allEntries.asLiveData()
 
-    val entries: LiveData<List<my_entry>> = repository.entries.asLiveData()
-
-    fun addEntry(journal_entry: my_entry) {
-        return viewModelScope.launch {  //wrapper addEntry method
-            repository.addEntry(journal_entry)
-        }
-    }
-}
-
-class EntryViewModelFactory(private val repository: EntryRepository) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>) : T {
-        if(modelClass.isAssignableFrom(EntryViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return EntryViewModel(repository) as T
-        }
-        throw IllegalArgumentException("unknown ViewModel class")
+    fun addEntry(new_entry: my_entry) = viewModelScope.launch {
+        repository.addEntry(new_entry)
     }
 }
