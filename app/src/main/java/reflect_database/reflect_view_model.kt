2ff137e69849de89ddc8d46dc401eeb73dcfb3d4
CSIWill <91165874+CSIWill@users.noreply.github.com>
2021-11-15 06:28:38 -0500

    Saving before merging branch branch

---
+++ app/src/main/java/reflect_database/reflect_view_model.kt
@@ -0,0 +1,23 @@
+package reflect_database
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+import reflect_database.my_entry
+
+class reflect_view_model(private val repository: reflect_repository) : ViewModel() {
+
+    val entries: LiveData<List<my_entry>> = repository.getEntries().asLiveData()
+
+    fun addEntry(journal_entry: my_entry) = viewModelScope.launch {  //wrapper addEntry method
+        repository.addEntry(journal_entry)
+    }
+}
+class reflect_view_model_factory(private val repository: reflect_repository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>) : T {
+        if(modelClass.isAssignableFrom(reflect_view_model::class.java)) {
+            @Suppress("unchecked cast")
+            return reflect_view_model(repository) as T
+        }
+        throw IllegalArgumentException("unknown ViewModel class")
+    }
+}
